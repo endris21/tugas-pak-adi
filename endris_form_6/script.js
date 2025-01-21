c8a6ff21505483bad37835fb1e7d34b9a090af7d
endris21 <endrisadamara54@gmail.com>
2025-01-21 13:29:59 +0700

    Add files via upload

---
+++ endris_form_6/script.js
@@ -0,0 +1,7 @@
+// Toggle Sidebar
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+});
